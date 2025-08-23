40382dfa29faa9a7d21b95518f84cb73038834b5
Eric Vennemeyer <henri.barun@gmail.com>
2025-08-22 17:30:39 -0700

    3.7 -- Making A Base

---
+++ Base/base.gd
@@ -0,0 +1,6 @@
+class_name Base
+extends Node3D
+
+
+func take_damage() -> void:
+	print("damage dealt to base!")
--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -2,6 +2,10 @@ extends PathFollow3D
 
 @export var speed: float = 5.0
 
+@onready var base: Base = get_tree().get_first_node_in_group("Base")
+
 
 func _process(delta: float) -> void:
 	progress += speed * delta
+	if progress_ratio == 1.0:
+		base.take_damage()
