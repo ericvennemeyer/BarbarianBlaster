bdaf9f3441e0a181fc228b7e5115fa9703a810e9
Eric Vennemeyer <henri.barun@gmail.com>
2025-08-25 14:32:02 -0700

    3.8 -- Health and Labels, 3.9 -- Introducing Properties

--- Base/base.gd
+++ Base/base.gd
@@ -2,5 +2,21 @@ class_name Base
 extends Node3D
 
 
+@export var max_health: int = 5
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		label_3d.text = str(current_health)
+		print("Health changed")
+		if current_health < 1:
+			get_tree().reload_current_scene()
+
+@onready var label_3d: Label3D = $Label3D
+
+
+func _ready() -> void:
+	current_health = max_health
+
+
 func take_damage() -> void:
-	print("damage dealt to base!")
+	current_health -= 1
--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -1,5 +1,6 @@
 extends PathFollow3D
 
+
 @export var speed: float = 5.0
 
 @onready var base: Base = get_tree().get_first_node_in_group("Base")
@@ -9,3 +10,4 @@ func _process(delta: float) -> void:
 	progress += speed * delta
 	if progress_ratio == 1.0:
 		base.take_damage()
+		set_process(false)
