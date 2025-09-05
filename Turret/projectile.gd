09fc1a9212f36b3c99c254d5944a7f69a5dae8e5
Eric Vennemeyer <henri.barun@gmail.com>
2025-09-05 13:46:47 -0700

    3.17 -- Introducing Timers

--- Turret/projectile.gd
+++ Turret/projectile.gd
@@ -8,3 +8,7 @@ var direction: Vector3 = Vector3.FORWARD
 
 func _physics_process(delta: float) -> void:
 	position += speed * direction * delta
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
--- Turret/turret.gd
+++ Turret/turret.gd
@@ -3,7 +3,10 @@ extends Node3D
 
 @export var projectile: PackedScene
 
+@onready var projectile_spawn_point: Marker3D = $TurretBase/TurretTop/BarrelBase/ProjectileSpawnPoint
 
-func _ready() -> void:
+
+func _on_timer_timeout() -> void:
 	var new_projectile = projectile.instantiate()
+	new_projectile.global_position = projectile_spawn_point.global_position
 	add_child(new_projectile)
