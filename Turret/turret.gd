ae97267ff99690d3d6556db26da255eeabc93499
Eric Vennemeyer <henri.barun@gmail.com>
2025-09-05 14:57:53 -0700

    3.20 -- For Loops and Targeting

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,19 +1,35 @@
 extends Node3D
 
 var enemy_path: Path3D
+var target: PathFollow3D
 
+@export var turret_range: float = 10.0
 @export var projectile: PackedScene
 
 @onready var projectile_spawn_point: Marker3D = $TurretBase/TurretTop/BarrelBase/ProjectileSpawnPoint
 
 
 func _physics_process(delta: float) -> void:
-	var enemy = enemy_path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+	target = find_best_target()
+	if target:
+		look_at(target.global_position, Vector3.UP, true)
 
 
 func _on_timer_timeout() -> void:
-	var new_projectile = projectile.instantiate()
-	new_projectile.global_position = projectile_spawn_point.global_position
-	add_child(new_projectile)
-	new_projectile.direction = global_transform.basis.z
+	if target:
+		var new_projectile = projectile.instantiate()
+		new_projectile.global_position = projectile_spawn_point.global_position
+		add_child(new_projectile)
+		new_projectile.direction = global_transform.basis.z
+
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progress = 0
+	for enemy in enemy_path.get_children():
+		var enemy_distance = global_position.distance_to(enemy.global_position)
+		if enemy is PathFollow3D:
+			if enemy.progress > best_progress and enemy_distance <= turret_range:
+				best_target = enemy
+				best_progress = enemy.progress
+	return best_target
