fc2064b238b501c8cfe249cc2beee971d4e2dd0d
Eric Vennemeyer <henri.barun@gmail.com>
2025-09-05 14:28:23 -0700

    3.19 -- Damaging Enemies

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -1,9 +1,20 @@
 extends PathFollow3D
 
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		print(current_health)
+		if current_health < 1:
+			queue_free()
 
+@export var max_health: int = 50
 @export var speed: float = 5.0
 
 @onready var base: Base = get_tree().get_first_node_in_group("Base")
+
+
+func _ready() -> void:
+	current_health = max_health
 
 
 func _process(delta: float) -> void:
