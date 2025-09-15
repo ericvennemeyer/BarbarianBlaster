519d575651ce7112687e7855549767e92d748d2c
Eric Vennemeyer <henri.barun@gmail.com>
2025-09-15 15:04:49 -0700

    3.26 -- Scaling Enemy Health

--- DiffcultyManager/difficulty_manager.gd
+++ DiffcultyManager/difficulty_manager.gd
@@ -1,14 +1,18 @@
 extends Node
 
 
+signal stop_spawning_enemies
+
 @export var game_length: float = 30.0
 @export var spawn_time_curve: Curve
+@export var enemy_health_curve: Curve
 
 @onready var timer: Timer = $Timer
 
 
 func _ready() -> void:
 	timer.start(game_length)
+	timer.timeout.connect(stop_spawning_enemies.emit)
 
 
 func _process(delta: float) -> void:
@@ -21,3 +25,7 @@ func game_progress_ratio() -> float:
 
 func get_spawn_time() -> float:
 	return spawn_time_curve.sample(game_progress_ratio())
+
+
+func get_enemy_health() -> int:
+	return round(enemy_health_curve.sample(game_progress_ratio()))
